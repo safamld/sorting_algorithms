1f6b35576b7d0a692b3f56ae3714125352bf19b2
safa <safaaamd7@gmail.com>
2024-04-11 10:08:44 -0700

    advanced

--- sort.h
+++ sort.h
@@ -29,4 +29,6 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
+/*advanced */
+void shell_sort(int *array, size_t size);
 #endif
