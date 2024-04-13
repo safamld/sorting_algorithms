6a2a70ab8abbc8c358b69d2cfa9f8350e7f8439e
safa <safaaamd7@gmail.com>
2024-04-13 11:19:59 -0700

    advanced

--- sort.h
+++ sort.h
@@ -32,4 +32,10 @@ void quick_sort(int *array, size_t size);
 /*advanced */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif
